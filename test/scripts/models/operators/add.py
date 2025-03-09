b7e08e7e753efa9692d23268e83db76715732ffd
Alwyn Bester <alwyn@zoclee.com>
2025-03-09 18:27:03 +0200

    Added scripts used to generate ONNX test files with.

---
+++ test/scripts/models/operators/add.py
@@ -0,0 +1,33 @@
+import onnx
+import onnx.helper as helper
+import onnx.numpy_helper as numpy_helper
+
+# Define input tensor/s
+input1 = helper.make_tensor_value_info('input1', onnx.TensorProto.FLOAT, [None, None])
+input2 = helper.make_tensor_value_info('input2', onnx.TensorProto.FLOAT, [None, None])
+
+# Define output tensor/s
+output1 = helper.make_tensor_value_info('output1', onnx.TensorProto.FLOAT, [None, None])
+
+# Define the node
+node1 = helper.make_node(
+    'Add',  # Operator name
+    ['input1', 'input2'],  # Inputs
+    ['output1']  # Output
+)
+
+# Create the graph
+graph = helper.make_graph(
+    [node1],  # Nodes
+    'Add',  # Graph name
+    [input1, input2],  # Inputs
+    [output1]  # Outputs
+)
+
+# Create the model
+model = helper.make_model(graph, producer_name='add')
+
+# Save the model to a file
+onnx.save(model, 'add.onnx')
+
+print("ONNX model saved")
